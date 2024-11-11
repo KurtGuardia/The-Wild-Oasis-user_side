5849d7a8c0781d1acfcd46f741f9c6fd5cfba645
Kurt Guardia <kurtguardia@gmail.com>
2024-11-11 19:28:00 +0100

    worked on reservation selection and reset with React Context

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import CabinList from '@/app/_components/CabinList'
 import { Suspense } from 'react'
 import Spinner from '../_components/Spinner'
 import Filter from '../_components/Filter'
+import ReservationReminder from '../_components/ReservationReminder'
 
 // export const revalidate = 3600 Doesn't work because it is no longer static due to the filter
 
@@ -34,6 +35,7 @@ export default function Page({ searchParams }) {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   )
--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from '@/app/_components/Navigation'
 import './_styles/globals.css'
 import { Josefin_Sans } from 'next/font/google'
 import Header from './_components/Header'
+import { ReservationProvider } from './_components/ReservationContext'
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -28,7 +29,9 @@ export default function RootLayout({ children }) {
 
         <div className='flex-1 px-8 py-12 grid'>
           <main className='max-w-7xl mx-auto w-full'>
-            {children}
+            <ReservationProvider>
+              {children}
+            </ReservationProvider>
           </main>
         </div>
       </body>
