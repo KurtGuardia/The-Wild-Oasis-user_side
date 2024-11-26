c86bd17d9860243c440813637dc4edfa7c7694d7
Kurt Guardia <kurtguardia@gmail.com>
2024-11-26 19:36:53 -0400

    Set reservations based on logged user, and created server action for dleting reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,11 +3,12 @@
 import { revalidatePath } from 'next/cache'
 import { auth, signIn, signOut } from './auth'
 import { supabase } from './supabase'
+import { getBookings } from './data-service'
 
 export async function updateguest(formData) {
   const session = await auth()
 
-  if (!session) throw new Error('You myust be logged in')
+  if (!session) throw new Error('You must be logged in')
 
   const nationalID = formData.get('nationalID')
   const [nationality, countryFlag] = formData
@@ -31,6 +32,33 @@ export async function updateguest(formData) {
   if (error) throw new Error('Guest could not be updated')
 
   revalidatePath('/account/profile')
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth()
+
+  if (!session) throw new Error('You must be logged in')
+
+  const guestBookings = await getBookings(
+    session.user.guestId,
+  )
+  const guestBookingIds = guestBookings.map(
+    (booking) => booking.id,
+  )
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error(
+      'You are not allowed to delete this booking',
+    )
+
+  const { error } = await supabase
+    .from('bookings')
+    .delete()
+    .eq('id', bookingId)
+
+  if (error) throw new Error('Booking could not be deleted')
+
+  revalidatePath('/account/reservations')
 }
 
 export async function signInAction() {
