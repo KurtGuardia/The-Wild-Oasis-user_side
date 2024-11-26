29d85f4676f5b0a2b453bd9d2c078e25c3fffa40
Kurt Guardia <kurtguardia@gmail.com>
2024-11-26 18:18:24 -0400

    set server actions to update profile

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,17 +1,34 @@
 'use client'
 
 import { useState } from 'react'
+import { useFormStatus } from 'react-dom'
+import { updateguest } from '../_lib/actions'
 
-export default function UpdateProfileForm({ children }) {
+export default function UpdateProfileForm({
+  guest,
+  children,
+}) {
   const [count, setCount] = useState()
-  const countryFlag = 'pt.jpg'
+
+  const {
+    fullName,
+    email,
+    nationality,
+    nationaID,
+    countryFlag,
+  } = guest
 
   return (
-    <form className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
+    <form
+      action={updateguest}
+      className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'
+    >
       <div className='space-y-2'>
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name='fullName'
           className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
         />
       </div>
@@ -20,6 +37,8 @@ export default function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
+          name='email'
           className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
         />
       </div>
@@ -44,16 +63,28 @@ export default function UpdateProfileForm({ children }) {
           National ID number
         </label>
         <input
+          defaultValue={nationaID}
           name='nationalID'
           className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
         />
       </div>
 
       <div className='flex justify-end items-center gap-6'>
-        <button className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  )
+}
+
+function Button() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
+      disabled={pending}
+    >
+      {pending ? 'Updating...' : 'Update profile'}
+    </button>
   )
 }
