06d54f0c825b325406de779f71319f13fd9cedc2
Kurt Guardia <kurtguardia@gmail.com>
2024-11-27 20:53:32 -0400

    Delete and edit reservation Server Action and implementation done

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { useFormStatus } from 'react-dom'
 import { updateguest } from '../_lib/actions'
+import SubmitButton from './SubmitButton'
 
 export default function UpdateProfileForm({
   guest,
@@ -70,21 +71,10 @@ export default function UpdateProfileForm({
       </div>
 
       <div className='flex justify-end items-center gap-6'>
-        <Button />
+        <SubmitButton pendingLabel='Updating...'>
+          Update profile
+        </SubmitButton>
       </div>
     </form>
-  )
-}
-
-function Button() {
-  const { pending } = useFormStatus()
-
-  return (
-    <button
-      className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
-      disabled={pending}
-    >
-      {pending ? 'Updating...' : 'Update profile'}
-    </button>
   )
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,13 +1,13 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
 import { auth, signIn, signOut } from './auth'
-import { supabase } from './supabase'
 import { getBookings } from './data-service'
+import { supabase } from './supabase'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
-export async function updateguest(formData) {
+export async function updateGuest(formData) {
   const session = await auth()
-
   if (!session) throw new Error('You must be logged in')
 
   const nationalID = formData.get('nationalID')
@@ -20,8 +20,8 @@ export async function updateguest(formData) {
 
   const updateData = {
     nationality,
-    nationalID,
     countryFlag,
+    nationalID,
   }
 
   const { data, error } = await supabase
@@ -34,9 +34,37 @@ export async function updateguest(formData) {
   revalidatePath('/account/profile')
 }
 
-export async function deleteReservation(bookingId) {
+export async function createBooking(bookingData, formData) {
   const session = await auth()
+  if (!session) throw new Error('You must be logged in')
 
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData
+      .get('observations')
+      .slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: 'unconfirmed',
+  }
+
+  const { error } = await supabase
+    .from('bookings')
+    .insert([newBooking])
+
+  if (error) throw new Error('Booking could not be created')
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`)
+
+  redirect('/cabins/thankyou')
+}
+
+export async function deleteBooking(bookingId) {
+  const session = await auth()
   if (!session) throw new Error('You must be logged in')
 
   const guestBookings = await getBookings(
@@ -59,6 +87,53 @@ export async function deleteReservation(bookingId) {
   if (error) throw new Error('Booking could not be deleted')
 
   revalidatePath('/account/reservations')
+}
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get('bookingId'))
+
+  // 1) Authentication
+  const session = await auth()
+  if (!session) throw new Error('You must be logged in')
+
+  // 2) Authorization
+  const guestBookings = await getBookings(
+    session.user.guestId,
+  )
+  const guestBookingIds = guestBookings.map(
+    (booking) => booking.id,
+  )
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error(
+      'You are not allowed to update this booking',
+    )
+
+  // 3) Building update data
+  const updateData = {
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData
+      .get('observations')
+      .slice(0, 1000),
+  }
+
+  // 4) Mutation
+  const { error } = await supabase
+    .from('bookings')
+    .update(updateData)
+    .eq('id', bookingId)
+    .select()
+    .single()
+
+  // 5) Error handling
+  if (error) throw new Error('Booking could not be updated')
+
+  // 6) Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`)
+  revalidatePath('/account/reservations')
+
+  // 7) Redirecting
+  redirect('/account/reservations')
 }
 
 export async function signInAction() {
